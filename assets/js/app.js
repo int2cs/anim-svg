76705ff5fcd9912cda16e7a027b79f6d8fcd5cd2
int2cs <gabriel.gn87@protonmail.com>
2022-09-20 11:49:20 +0200

    add gsap.config for force3D test

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,11 @@
+gsap.config({
+    force3D: true
+})
 const tl = gsap.timeline()
 
 tl.to('.wrapper-container-img', {
     transform: "rotateX(0)",
-    duration: 2,
+    duration: 3,
     stagger: 0.1,
     ease: "elastic.out(1, 0.3)",
 })
