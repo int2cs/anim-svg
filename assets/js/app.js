b106fefff6d0bc23021b840f77aae87236000f6d
int2cs <gabriel.gn87@protonmail.com>
2022-09-20 12:12:57 +0200

    commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,17 @@
-gsap.config({
-    force3D: true
-})
+const btn = document.getElementById('btnPlay')
+const btnR = document.getElementById('btnReplay')
 const tl = gsap.timeline()
 
 tl.to('.wrapper-container-img', {
     transform: "rotateX(0)",
-    duration: 3,
-    stagger: 0.1,
+    duration: 2.5,
+    stagger: 0.15,
     ease: "elastic.out(1, 0.3)",
+}).pause()
+
+btn.addEventListener('click', ()=>{
+    tl.play()
+})
+btnR.addEventListener('click', ()=>{
+    tl.restart()
 })
